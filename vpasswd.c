e8ec4ef0962ced123eec19a21deca39acdb6cac3
Patrice <pat@tlk.biz>
2022-04-13 15:38:21 +0200

    add error message in vpasswd when using -r and a password on command line

--- vpasswd.c
+++ vpasswd.c
@@ -17,19 +17,18 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
  */
 
+#include <fcntl.h>
+#include <pwd.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
-#include <pwd.h>
-#include <sys/types.h>
 #include <sys/stat.h>
-#include <fcntl.h>
-#include <signal.h>
+#include <sys/types.h>
+#include <unistd.h>
 #include "config.h"
-#include "vpopmail.h"
 #include "vauth.h"
-
+#include "vpopmail.h"
 
 char Email[MAX_BUFF];
 char User[MAX_BUFF];
@@ -39,93 +38,96 @@ int apop;
 int RandomPw;
 
 void usage();
-void get_options(int argc,char **argv);
+void get_options(int argc, char **argv);
 
-int main(int argc, char *argv[])
-{
- int i;
+int main(int argc, char *argv[]) {
+  int i;
 
-	if( vauth_open( 1 )) {
-		vexiterror( stderr, "Initial open." );
-	}
+  if (vauth_open(1)) {
+    vexiterror(stderr, "Initial open.");
+  }
 
-	get_options(argc,argv);
+  get_options(argc, argv);
 
-        if ( (i = parse_email( Email, User, Domain, sizeof(User))) != 0 ) {
-            printf("Error: %s\n", verror(i));
-            vexit(i);
-        }
+  if ((i = parse_email(Email, User, Domain, sizeof(User))) != 0) {
+    printf("Error: %s\n", verror(i));
+    vexit(i);
+  }
 
-	if ( strlen(Passwd) <= 0 ) {
-		vgetpasswd(Email, Passwd, sizeof(Passwd));
-	}
+  if (strlen(Passwd) <= 0) {
+    vgetpasswd(Email, Passwd, sizeof(Passwd));
+  }
 
-	if ( (i=vpasswd( User, Domain, Passwd, apop )) != 0 ) {
-		printf("Error: %s\n", verror(i));
-		vexit(i);
-	}
-	if (RandomPw) printf("Random password: %s\n", Passwd);
-	return(vexit(0));
-
+  if ((i = vpasswd(User, Domain, Passwd, apop)) != 0) {
+    printf("Error: %s\n", verror(i));
+    vexit(i);
+  }
+  if (RandomPw) printf("Random password: %s\n", Passwd);
+  return (vexit(0));
 }
 
-void usage()
-{
-	printf("vpasswd: usage: [options] email_address [password]\n");
-	printf("options: -v (print version number)\n");
-	printf("         -r[len] (generate a len (default 12) char random password)\n");
+void usage() {
+  printf("vpasswd: usage: [options] email_address [password]\n");
+  printf("options: -v (print version number)\n");
+  printf(
+      "         -r[len] (generate a len (default 12) char random password)\n");
 }
 
-void get_options(int argc,char **argv)
-{
- int c;
- int errflag;
- extern int optind;
- extern char *optarg;
+void get_options(int argc, char **argv) {
+  int c;
+  int errflag;
+  extern int optind;
+  extern char *optarg;
 
-	memset(Email, 0, sizeof(Email));
-	memset(Passwd, 0, sizeof(Passwd));
-	memset(Domain, 0, sizeof(Domain));
-	apop = USE_POP;
-	RandomPw = 0;
+  memset(Email, 0, sizeof(Email));
+  memset(Passwd, 0, sizeof(Passwd));
+  memset(Domain, 0, sizeof(Domain));
+  apop = USE_POP;
+  RandomPw = 0;
 
-	errflag = 0;
-    while( !errflag && (c=getopt(argc,argv,"vr::")) != -1 ) {
-		switch(c) {
-			case 'v':
-				printf("version: %s\n", VERSION);
-				break;
-			case 'r':
-				RandomPw = 1;
-				if (optarg) {
-                    int sz = atoi(optarg);
-                    if (sz > 128) {
-                        printf("Error: %s\n", verror(VA_PASSWD_TOO_LONG));
-                        vexit(VA_PASSWD_TOO_LONG);
-                    }
-				    vrandom_pass (Passwd, atoi(optarg));
-				} else {
-    				vrandom_pass (Passwd, 12);
-    			}
-				break;
-			default:
-				errflag = 1;
-				break;
-		}
-	}
-
-	if ( optind < argc ) {
-	  snprintf(Email, sizeof(Email), "%s", argv[optind]);
-      	  ++optind;
+  errflag = 0;
+  while (!errflag && (c = getopt(argc, argv, "vr::")) != -1) {
+    switch (c) {
+      case 'v':
+        printf("version: %s\n", VERSION);
+        break;
+      case 'r':
+        RandomPw = 1;
+        if (optarg) {
+          int sz = atoi(optarg);
+          if (sz > 128) {
+            printf("Error: %s\n", verror(VA_PASSWD_TOO_LONG));
+            vexit(VA_PASSWD_TOO_LONG);
+          }
+          vrandom_pass(Passwd, atoi(optarg));
+        } else {
+          vrandom_pass(Passwd, 12);
         }
+        break;
+      default:
+        errflag = 1;
+        break;
+    }
+  }
 
-	if ( optind < argc ) {
-	  snprintf(Passwd, sizeof(Passwd), "%s", argv[optind]);
-	  ++optind;
-        }
+  if (optind < argc) {
+    snprintf(Email, sizeof(Email), "%s", argv[optind]);
+    ++optind;
+  }
 
-	if ( Email[0] == 0 ) { 
-		usage();
-		vexit(-1);
-	}
+  if (optind < argc) {
+    if (RandomPw == 0) {
+      snprintf(Passwd, sizeof(Passwd), "%s", argv[optind]);
+      ++optind;
+    } else {
+      printf(
+          "Error: don't use -r option when giving password on command line\n");
+      vexit(-1);
+    }
+  }
+
+  if (Email[0] == 0) {
+    usage();
+    vexit(-1);
+  }
 }
